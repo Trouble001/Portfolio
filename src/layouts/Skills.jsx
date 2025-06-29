82d4542b405fe281dfc75b8b9ab03547c63acd0c
staytrouble <gschaturvedi001@gmail.com>
2025-06-29 14:01:56 +0530

    Changed in some styles

--- src/layouts/Skills.jsx
+++ src/layouts/Skills.jsx
@@ -3,49 +3,49 @@ const Skills = () => {
     <div className="w-11/12 p-4 xl:p-0 flex flex-col items-start text-sm font-bold text-black/90 dark:text-white/80">
       <div className="w-full mb-4">
         <p className="">HTML</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[90%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">CSS</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[80%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">JavaScript</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[75%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">Tailwind</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[80%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">Bootstrap</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[75%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">React</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[85%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
 
       <div className="w-full mb-4">
         <p className="">Django</p>
-        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full">
+        <div className="w-full h-2 mt-1.5 bg-black/90 dark:bg-white/80 rounded-full shadow">
           <div className="w-[70%] bg-green-600 h-full rounded-l-full"></div>
         </div>
       </div>
