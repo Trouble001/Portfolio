cec2eb1cf995ef9572af0a0f28b8d10414f7a76d
staytrouble <gschaturvedi001@gmail.com>
2025-06-29 14:11:51 +0530

    Change styles for mobile view

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -6,9 +6,9 @@ const Navigation = () => {
       <div className="w-11/12 mx-auto h-full flex items-center justify-between">
         <a href="/" className="text-base sm:text-md md:text-lg lg:text-xl text-black/100 dark:text-white/100">Folio</a>
         <div className="">
-          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-2 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">Home</a>
-          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-2 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">About</a>
-          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-2 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">Projects</a>
+          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-3 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">Home</a>
+          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-3 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">About</a>
+          <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 mr-3 sm:mr-3 md:mr-4 lg:mr-5 xl:mr-6 hover:text-green-500 hover:border-b transition-all">Projects</a>
           <a href="/" className="text-xs sm:text-sm md:text-md lg:text-lg text-black/90 dark:text-white/90 hover:text-green-500 hover:border-b transition-all">Contact</a>
         </div>
         <ThemeToggle />
--- src/layouts/HeroSection.jsx
+++ src/layouts/HeroSection.jsx
@@ -7,8 +7,8 @@ const HeroSection = () => {
           <h3 className="text-3xl sm:text-3xl md:text-3xl lg:text-4xl xl:text-5xl font-bold text-black/100 dark:text-white/100">I'm <span className="text-green-600 dark:text-green-600">Pranesh Chaturvedi</span></h3>
           <p className="text-md sm:text-lg md:text-xl lg:text-2xl font-normal text-black/80 dark:text-white/80">Full Stack Web Developer</p>
           <div className="w-full flex flex-row items-center justify-start mt-2">
-            <button className="text-xs sm:text-xs md:text-sm lg-text-base outline bg-green-600 outline-green-600 text-white dark:text-black px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1 sm:py-1 md:py-1.5 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow mr-2">Contact Me</button>
-            <button className="text-xs sm:text-xs md:text-sm lg-text-base outline outline-green-600 text-green-600 px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1 sm:py-1 md:py-1.5 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow">Get Resume</button>
+            <button className="text-sm sm:text-sm md:text-base lg-text-base xl:text-md outline bg-green-600 outline-green-600 text-white dark:text-black px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow mr-2">Contact Me</button>
+            <button className="text-sm sm:text-sm md:text-base lg-text-base xl:text-md outline outline-green-600 text-green-600 px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow">Get Resume</button>
           </div>
         </div>
       </div>
