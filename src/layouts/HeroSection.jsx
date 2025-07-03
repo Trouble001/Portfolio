3b49233c2b8b52cf711edf5627b7fc39c7d08662
staytrouble <gschaturvedi001@gmail.com>
2025-07-03 15:02:42 +0530

    Fixing some styles

--- src/layouts/HeroSection.jsx
+++ src/layouts/HeroSection.jsx
@@ -19,9 +19,9 @@ const HeroSection = () => {
         </div>
       </div>
       <div className="w-full flex items-center justify-center relative order-1 lg:order-2">
-        <div className={`lg:w-5/12 md:w-3/12 md:h-10/12 lg:h-6/12 w-5/12 h-10/12 flex outline-6 ${themeColors[color].outline} ${themeColors[color].bg} absolute rounded grayscale-75`}>
+        <div className={`lg:w-5/12 md:w-3/12 md:h-10/12 lg:h-6/12 w-5/12 h-10/12 flex outline-6 ${themeColors[color].outline} ${themeColors[color].bg} absolute rounded-md grayscale-75`}>
         </div>
-        <img className="lg:w-6/12 md:w-4/12 md:h-full lg:h-7/12 w-6/12 h-full drop-shadow-md absolute md:-ml-22 md:-mt-7 lg:-ml-20 lg:-mt-12 -ml-16 -mt-7.5 grayscale rounded " src={myphoto} alt="Pranesh Chaturvedi" />
+        <img className="lg:w-6/12 md:w-4/12 md:h-full lg:h-7/12 w-6/12 h-full drop-shadow-md absolute md:-ml-22 md:-mt-7 lg:-ml-20 lg:-mt-12 -ml-12 -mt-8 grayscale rounded-md" src={myphoto} alt="Pranesh Chaturvedi" />
       </div>
     </Element>
   )
