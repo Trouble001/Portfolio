0de13ece8a39edc62478623c67b0e6a0879ed381
staytrouble <gschaturvedi001@gmail.com>
2025-07-04 17:17:55 +0530

    Changing margins

--- src/layouts/AboutSection.jsx
+++ src/layouts/AboutSection.jsx
@@ -8,13 +8,13 @@ const AboutSection = () => {
   return (
     <Element name="about" className="w-full min-h-screen grid gap-8 xl:gap-0 grid-cols-1 xl:grid-cols-2 border-b-2 items-center justify-center border-black/30 dark:border-white/30">
       <div className="w-full flex flex-col items-center justify-center pt-18 pb-4">
-        <div className="w-11/12">
+        <div className="w-full">
           <h1 className={`text-left text-lg sm:text-xl md:text-2xl lg:text-3xl xl:4xl transition-colors duration-500 font-bold ${themeColors[color].text}`}>— About Me</h1>
         </div>
-        <div className="w-10/12 mt-4 pb-4 lg:pr-4 border-b border-black/30 dark:border-white/30">
+        <div className="w-11/12 mt-4 pb-4 lg:pr-4 border-b border-black/30 dark:border-white/30">
           <p className="text-justify text-sm sm:text-base md:text-base lg:text-md xl:text-md text-black/80 dark:text-white/70 dark:font-normal">Hi, I'm Praanesh Chaturvedi — a passionate Full-Stack Developer with a strong foundation in both front-end and back-end technologies. I specialize in building dynamic, responsive, and user-friendly web applications using tools like React, JavaScript, HTML/CSS, Python, and MySQL. My experience includes developing authentication systems, CRUD applications, REST APIs, and interactive UIs. Currently, I work as a computer instructor, where I not only teach programming concepts but also guide students in real-world projects. I'm continuously expanding my skills in areas like DSA, React Native, and AWS to stay ahead in the evolving tech landscape.<br/><br/>I enjoy turning ideas into code, solving problems with clean architecture, and collaborating on meaningful projects. My goal is to contribute to impactful products and be a part of a team that values innovation, performance, and user experience. When I’m not coding, I’m probably experimenting with new tech stacks or helping others learn to code.</p>
         </div>
-        <div className="w-10/12 mt-4 grid grid-cols-2 text-xs sm:text-sm md:text-base lg:text-md xl:text-md">
+        <div className="w-11/12 mt-4 grid grid-cols-2 text-xs sm:text-sm md:text-base lg:text-md xl:text-md">
           <div>
             <p className={`${themeColors[color].text} transition-colors duration-500 font-medium`}>Name</p>
             <p className="text-black/90 dark:text-white/70">Pranesh Chaturvedi</p>
@@ -30,7 +30,7 @@ const AboutSection = () => {
           </div>
           
         </div>
-        <div className="w-10/12 mt-6 flex flex-row items-center justify-start">
+        <div className="w-11/12 mt-6 flex flex-row items-center justify-start">
           <a href="../public/resume.pdf" download className="text-xs md:text-sm lg:text-base xl:text-md shadow text-black/80 dark:text-white/70 outline outline-black/30 dark:outline-white/30 px-8 sm:px-8 md:px-10 lg:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full">Get Resume</a>
         </div>
       </div>
--- src/layouts/HeroSection.jsx
+++ src/layouts/HeroSection.jsx
@@ -1,6 +1,6 @@
 import { useContext } from "react";
 import { ThemeContext } from "../context/ThemeContext";
-import { Element } from "react-scroll";
+import { Element, Link } from "react-scroll";
 // import myphoto from "../assets/myimage.png";
 
 const HeroSection = () => {
@@ -12,7 +12,7 @@ const HeroSection = () => {
         <h3 className="text-3xl sm:text-3xl md:text-4xl lg:text-4xl xl:text-5xl font-bold text-black/100 dark:text-white/100 transition-colors duration-500">I'm <span className={`transition-colors duration-500 ${themeColors[color].text}`}>Pranesh Chaturvedi</span></h3>
         <p className="text-md sm:text-lg md:text-2xl lg:text-2xl xl:text-2xl font-normal text-black/80 dark:text-white/80">Full Stack Web Developer</p>
         <div className="w-full flex flex-row items-center justify-start mt-2">
-          <button className={`text-sm sm:text-sm md:text-base lg-text-base xl:text-md transition-colors duration-500 ${themeColors[color].bg} outline ${themeColors[color].outline} text-white dark:text-black px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow mr-3 lg:mr-4`}>Contact Me</button>
+          <Link to="contact" smooth={true} duration={300} className={`text-sm sm:text-sm md:text-base lg-text-base xl:text-md transition-colors duration-500 ${themeColors[color].bg} outline ${themeColors[color].outline} text-white dark:text-black px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow mr-3 lg:mr-4`}>Contact Me</Link>
           <a href="../public/resume.pdf" download className={`text-sm sm:text-sm md:text-base lg-text-base xl:text-md transition-colors duration-500 outline ${themeColors[color].outline} ${themeColors[color].text}  px-6 sm:px-7 md:px-8 lg:px-10 xl:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full flex items-center justify-center shadow`}>Get Resume</a>
         </div>
       </div>
