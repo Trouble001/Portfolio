13430476b823ead6a02c090de68abecc97e2f0b8
staytrouble <gschaturvedi001@gmail.com>
2025-07-05 15:06:29 +0530

    Added framer motion opacity and duration

--- src/layouts/AboutSection.jsx
+++ src/layouts/AboutSection.jsx
@@ -10,13 +10,13 @@ const AboutSection = () => {
   const { ref, inView } = useInView({ triggerOnce: true, threshold: 0.2 });
 
   return (
-    <Element name="about" className="w-full min-h-screen grid gap-8 xl:gap-0 grid-cols-1 xl:grid-cols-2 border-b-2 items-center justify-center border-black/30 dark:border-white/30">
-      <motion.div
+    <motion.Element
         ref={ref}
-        initial={{ opacity: 0, y: 50 }}
+        initial={{ opacity: 0, y: 300 }}
         animate={inView ? { opacity: 1, y: 0 } : {}}
         transition={{ duration: 0.6, ease: "easeOut" }}
-        className="w-full flex flex-col items-center justify-center pt-18 pb-4">
+        name="about" className="w-full min-h-screen grid gap-8 xl:gap-0 grid-cols-1 xl:grid-cols-2 border-b-2 items-center justify-center border-black/30 dark:border-white/30">
+      <div className="w-full flex flex-col items-center justify-center pt-18 pb-4">
         <div className="w-full">
           <h1 className={`text-left text-lg sm:text-xl md:text-2xl lg:text-3xl xl:4xl transition-colors duration-500 font-bold ${themeColors[color].text}`}>— About Me</h1>
         </div>
@@ -42,19 +42,14 @@ const AboutSection = () => {
         <div className="w-11/12 mt-6 flex flex-row items-center justify-start">
           <a href="../public/resume.pdf" download className="text-xs md:text-sm lg:text-base xl:text-md shadow text-black/80 dark:text-white/70 outline outline-black/30 dark:outline-white/30 px-8 sm:px-8 md:px-10 lg:px-12 py-1.5 sm:py-1.5 md:py-2 lg:py-2 xl:py-2 cursor-pointer rounded-full">Get Resume</a>
         </div>
-      </motion.div>
+      </div>
 
-      <motion.div
-        ref={ref}
-        initial={{ opacity: 0, y: 50 }}
-        animate={inView ? { opacity: 1, y: 0 } : {}}
-        transition={{ duration: 0.6, ease: "easeOut" }}
-        className="w-full flex flex-col xl:flex-row items-center justify-center">
+      <div className="w-full flex flex-col xl:flex-row items-center justify-center">
         <div className="xl:w-1/12 xl:-rotate-90 xl:pb-4 mx-auto font-medium xl:font-bold text-lg lg:text-2xl tracking-widest text-black/50 dark:text-white/30"><h3>SKILLS</h3></div>
         <Skills />
-      </motion.div>
+      </div>
 
-    </Element>
+    </motion.Element>
   )
 }
 
